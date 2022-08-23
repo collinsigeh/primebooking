c55e3fe044ccbdfada7e0ea080e0f91914b8d5fe
Collins Igeh <igehac@gmail.com>
2022-08-23 11:14:53 +0100

    created the middleware and routes

---
+++ api/routes/hotels.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+//CREATE
+router.get("/", (req, res) => {
+
+});
+
+export default router;
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./api/routes/auth.js";
+import usersRoute from "./api/routes/users.js";
+import hotelsRoute from "./api/routes/hotels.js";
+import roomsRoute from "./api/routes/rooms.js";
+
 const app = express();
 dotenv.config();
 
@@ -20,6 +25,12 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("connected", () => {
     console.log("mongoDB connected!")
 })
+
+// middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8800, () => {
     connect();
