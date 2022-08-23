1c9f07465a6a586ecea693abf851bd7bcc2e94ed
Collins Igeh <igehac@gmail.com>
2022-08-23 16:54:27 +0100

    refactored route code into controllers methods

---
+++ api/controllers/hotel.js
@@ -0,0 +1,50 @@
+import Hotel from "../models/Hotel.js";
+
+export const createHotelController = async (req, res, next) => {
+    const newHotel = new Hotel(req.body);
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    }catch(err) {
+        next(err);
+    }
+}
+
+export const updateHotelController = async (req, res, next) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatedHotel);
+    }catch(err) {
+        next(err);
+    }
+}
+
+export const deleteHotelController = async (req, res, next) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted!");
+    }catch(err) {
+        next(err);
+    }
+}
+
+export const getHotelController = async (req, res, next) => {
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    }catch(err) {
+        next(err);
+    }
+}
+
+export const getHotelsController = async (req, res, next) => {
+    const newHotel = new Hotel(req.body);
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    }catch(err) {
+        next(err);
+    }
+}
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,42 +1,21 @@
 import express from "express";
+import {
+    createHotelController, 
+    deleteHotelController, 
+    updateHotelController 
+} from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req, res) => {
-
-    const newHotel = new Hotel(req.body);
-
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    }catch(err) {
-        res.status(500).json(err);
-    }
-});
+router.post("/", createHotelController);
 
 //UPDATE
-router.put("/:id", async (req, res) => {
-    
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-        res.status(200).json(updatedHotel);
-    }catch(err) {
-        res.status(500).json(err);
-    }
-});
+router.put("/:id", updateHotelController);
 
 //DELETE
-router.delete("/:id", async (req, res) => {
-    
-    try {
-        const updatedHotel = await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("Hotel has been deleted!");
-    }catch(err) {
-        res.status(500).json(err);
-    }
-});
+router.delete("/:id", deleteHotelController);
 
 //GET
 router.get("/:id", async (req, res) => {
